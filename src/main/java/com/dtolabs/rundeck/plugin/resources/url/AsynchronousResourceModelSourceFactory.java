7612734cf513ff260172a26321c85d640dee5f86
Chuck Scott <connaryscott@gmail.com>
2012-07-10 15:01:48 -0700

    added configurable support for yaml or xml

--- src/main/java/com/dtolabs/rundeck/plugin/resources/url/AsynchronousResourceModelSourceFactory.java
+++ src/main/java/com/dtolabs/rundeck/plugin/resources/url/AsynchronousResourceModelSourceFactory.java
@@ -5,6 +5,7 @@ import com.dtolabs.rundeck.core.plugins.Plugin;
 import com.dtolabs.rundeck.core.plugins.configuration.*;
 import com.dtolabs.rundeck.core.resources.ResourceModelSource;
 import com.dtolabs.rundeck.core.resources.ResourceModelSourceFactory;
+import com.dtolabs.rundeck.core.resources.format.UnsupportedFormatException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
@@ -17,8 +18,15 @@ public class AsynchronousResourceModelSourceFactory implements ResourceModelSour
     public static final Logger logger = Logger.getLogger(AsynchronousResourceModelSourceFactory.class);
 
     public static final String PROVIDER_NAME = "asynchronous-nodes-provider";
+
+    public static final String RESOURCES_FORMAT_KEY = "resourcesFormatKey";
+    public static final String RESOURCES_FORMAT_XML = "xml";
+    public static final String RESOURCES_FORMAT_YML = "yml";
+    public static final String RESOURCES_FORMAT_DEFAULT = RESOURCES_FORMAT_XML;
+
     public static final String RESOURCES_URL_KEY = "resourcesUrlKey";
     public static final String RESOURCES_URL_DEFAULT = "http://localhost/resources.xml";
+
     public static final String REFRESH_INTERVAL_KEY = "refreshIntervalKey";
     public static final int REFRESH_INTERVAL_DEFAULT = 30;
 
@@ -35,7 +43,13 @@ public class AsynchronousResourceModelSourceFactory implements ResourceModelSour
     public ResourceModelSource createResourceModelSource(final Properties properties) throws ConfigurationException {
         if (null == singleton) {
         logger.debug("constructing factory singleton");
-        final XMLResourceModelSource xmlResourceModelSource = new XMLResourceModelSource(properties);
+        XMLResourceModelSource xmlResourceModelSource = null;
+        try {
+           xmlResourceModelSource = new XMLResourceModelSource(this.framework, properties);
+        } catch (UnsupportedFormatException e) {
+           logger.error("caught UnsupportedFormatException exception: " +  e.getMessage());
+           throw new ConfigurationException(e);
+        }
         xmlResourceModelSource.validate();
         this.singleton = xmlResourceModelSource;
         return xmlResourceModelSource;
@@ -47,6 +61,12 @@ public class AsynchronousResourceModelSourceFactory implements ResourceModelSour
  
 
     static {
+
+        List<String> resourceFormats = new ArrayList();
+        resourceFormats.add(RESOURCES_FORMAT_XML);
+        resourceFormats.add(RESOURCES_FORMAT_YML);
+
+        descriptionProperties.add(PropertyUtil.freeSelect(RESOURCES_FORMAT_KEY, "resources format", "source resource format (default is " + RESOURCES_FORMAT_DEFAULT + ")", true, RESOURCES_FORMAT_DEFAULT, resourceFormats));
         descriptionProperties.add(PropertyUtil.string(RESOURCES_URL_KEY, "resources url", "source resource url", false, RESOURCES_URL_DEFAULT));
         descriptionProperties.add(PropertyUtil.integer(REFRESH_INTERVAL_KEY, "Refresh Interval", "Minimum time in seconds between url requests (default is " + Integer.toString(REFRESH_INTERVAL_DEFAULT) + ")", false,  Integer.toString(REFRESH_INTERVAL_DEFAULT)));
     }
--- src/main/java/com/dtolabs/rundeck/plugin/resources/url/XMLResourceModelSource.java
+++ src/main/java/com/dtolabs/rundeck/plugin/resources/url/XMLResourceModelSource.java
@@ -2,46 +2,56 @@ package com.dtolabs.rundeck.plugin.resources.url;
 
 import java.io.*;
 import java.util.Properties;
+import com.dtolabs.rundeck.core.common.Framework;
 import com.dtolabs.rundeck.core.resources.ResourceModelSource;
 import com.dtolabs.rundeck.core.resources.ResourceModelSourceException;
 import com.dtolabs.rundeck.core.resources.format.ResourceFormatParserException;
-import com.dtolabs.rundeck.core.resources.format.ResourceXMLFormatParser;
+import com.dtolabs.rundeck.core.resources.format.ResourceFormatParserService;
+import com.dtolabs.rundeck.core.resources.format.ResourceFormatParser;
+import com.dtolabs.rundeck.core.resources.format.UnsupportedFormatException;
 import com.dtolabs.rundeck.core.plugins.configuration.ConfigurationException;
 import com.dtolabs.rundeck.core.common.INodeSet;
 import org.apache.log4j.Logger;
 
 public class XMLResourceModelSource implements ResourceModelSource {
 
+    private ResourceFormatParser resourceFormatParser; 
+
     public static final Logger logger = Logger.getLogger(XMLResourceModelSource.class);
 
    private AsynchronousWorker asynchronousWorker;
-   public XMLResourceModelSource(final Properties configuration) {
+   public XMLResourceModelSource(Framework framework, final Properties configuration) throws UnsupportedFormatException {
 
         logger.debug("constructing XMLResourceModelSource");
 
+      String resourcesFormat = configuration.getProperty(AsynchronousResourceModelSourceFactory.RESOURCES_FORMAT_KEY);
       String resourcesUrl = configuration.getProperty(AsynchronousResourceModelSourceFactory.RESOURCES_URL_KEY);
       int refreshInterval = Integer.parseInt(configuration.getProperty(AsynchronousResourceModelSourceFactory.REFRESH_INTERVAL_KEY));
       this.asynchronousWorker = new AsynchronousWorker(resourcesUrl, refreshInterval);
       this.asynchronousWorker.initialize();
+      ResourceFormatParser resourceFormatParser = null;
+      logger.debug("RESOURCES_FORMAT_KEY is: " +  AsynchronousResourceModelSourceFactory.RESOURCES_FORMAT_KEY);
+      logger.debug("resourcesFormat: " +  resourcesFormat);
+      resourceFormatParser = new ResourceFormatParserService(framework).getParserForFileExtension(resourcesFormat);
+      this.resourceFormatParser = resourceFormatParser;
    }
 
    public synchronized INodeSet getNodes() throws ResourceModelSourceException {
 
         logger.debug("entering XMLResourceModelSource.getNodes()");
 
-      ResourceXMLFormatParser resourceFormatParser = new ResourceXMLFormatParser();
-
       InputStream is = null;
       INodeSet iNodeSet = null;
       try {
          is = new ByteArrayInputStream(this.asynchronousWorker.getUrlWorker().getVolatileResourcesXml().getBytes("UTF-8"));
-         iNodeSet = resourceFormatParser.parseDocument(is);
+         iNodeSet = this.resourceFormatParser.parseDocument(is);
       } catch (IOException e) {
          e.printStackTrace();
-         throw new ResourceModelSourceException("caught IOException");
+         logger.error("Caught IOException: " + e.getMessage());
+         throw new ResourceModelSourceException(e);
       } catch (ResourceFormatParserException e) {
-         e.printStackTrace();
-         throw new ResourceModelSourceException("caught ResourceFormatParserException");
+         logger.error("Caught ResourceFormatParserException: " + e.getMessage());
+         throw new ResourceModelSourceException(e);
       }
 
         logger.debug("leaving XMLResourceModelSource.getNodes()");
